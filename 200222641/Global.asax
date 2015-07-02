a6d059cbd745c484f2383441aed5f072e8437aa1
jearle <jearle@earlesystems.com>
2015-07-02 16:22:27 -0400

    First

---
+++ 200222641/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200222641.MvcApplication" Language="C#" %>
---
+++ 200222641/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200222641.Startup))]
+namespace _200222641
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
